55a6a657b10c8e0352bbbd018f38a3ef21a44762
Fran <francesca-may@users.noreply.github.com>
2018-02-23 11:34:54 +0000

    23/02/2018 - 11:31
    Better responsive compatibility & clearer styling, including background image size made smaller for better loading and compatibility, carousel indicators made more visible and smooth scroll edited to make more sense. Mobile carousel used to jump to top when clicking through slides which was jarring, this problem no longer exists.

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,13 +4,14 @@ $(document).ready(
         $('[data-toggle="tooltip"]').tooltip()
     }),
 
-    //Smooth scroll
-    $(document).on('click', 'a[href^="#"]', function (event) {
+    //Smooth scroll on mainMenu items
+    $('a[href^="#work"] , a[href^="#about"] , a[href^="#contact"]').on('click', function (event) {
         event.preventDefault();
         $('html, body').animate({ scrollTop: $($.attr(this, 'href')).offset().top - 50 }, 900);
     }),
-    $(document).on('click', 'a[href^="#hero"]', function (event) {
+    //Scroll higher to get to page top
+    $('a[href^="#hero"]').on('click', function (event) {
         event.preventDefault();
         $('html, body').animate({ scrollTop: $($.attr(this, 'href')).offset().top - 110 }, 900);
-    })
+    }),
 );
