affecdcf80e4d1f447059dc82e3b84f1fc9bcbaf
Fran <francesca-may@users.noreply.github.com>
2019-11-21 18:56:22 +0000

    Carousel added

--- new_website/js/main.js
+++ new_website/js/main.js
@@ -1,29 +1,31 @@
 //Nav movement
 $(document).ready(function(){
-    $(window).scroll(function() { // check if scroll event happened
-      if ($(document).scrollTop() > 40) { // check if user scrolled more than 50 from top of the browser window
-          $("header .container").css({
-            "transform" : "scale(.85)"
-          })
-          $("header").css({
-              "padding" : ".3em"
-          })
-      } else {
-          $("header .container").css({
-            "transform" : "scale(1)"
-          })
-          $("header").css({
-              "padding" : "1em"
-          })
-      }
-    });
-
-    $(".burger").on("click", function(){
-      if($(".mainMenuItem").css('display') == 'none'){
-          $(".mainMenuItem").slideDown("slow");
-      } else {
-          $(".mainMenuItem").slideUp("slow");
-      }
-    });
-
+  $(".burger").on("click", function(){
+    if($(".mainMenuItem").css('display') == 'none'){
+        $(".mainMenuItem").slideDown("slow");
+    } else {
+        $(".mainMenuItem").slideUp("slow");
+    }
   });
+  
+  $(window).scroll(function() { // check if scroll event happened
+    if ($(document).scrollTop() > 40) { // check if user scrolled more than 50 from top of the browser window
+        $("header .container").css({
+          "transform" : "scale(.85)"
+        })
+        $("header").css({
+            "padding" : ".3em"
+        })
+    } else {
+        $("header .container").css({
+          "transform" : "scale(1)"
+        })
+        $("header").css({
+            "padding" : "1em"
+        })
+    }
+  });
+
+  $('.carousel').carousel();
+
+});
