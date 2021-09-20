3718ea3cb1b0b8932dfaba3fdc1827a235c2d88a
Fran <francesca-may@users.noreply.github.com>
2021-09-20 23:12:43 +0100

    Complete site overhaul
    I'll be updating this site intermittently in the coming weeks now that I have a few fun projects under my belt.
    
    For now, it will feature quick links to some interesting experiences.

---
+++ index.js
@@ -0,0 +1,7 @@
+/**
+ * HTML Elements
+ */
+ const galaxyProjectButton = document.getElementById("galaxyProjectButton");
+ galaxyProjectButton.addEventListener("click", () => {
+     window.location = 'pages/galaxy/galaxy.html';
+ });
